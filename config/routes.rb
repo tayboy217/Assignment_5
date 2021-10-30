0881c47a2ac69a88a30ca2ac4e2fd6004a21b2aa
tayboy217 <tayboy217@icloud.com>
2021-10-30 15:11:23 +0000

    Update

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,17 @@
 Rails.application.routes.draw do
+  get 'relationships/followings'
+  get 'relationships/followers'
   devise_for :users
 
   root to:'homes#top'
   get 'home/about' => 'homes#about'
 
-  resources :users,only: [:show,:index,:edit,:update,]
+  resources :users,only: [:show,:index,:edit,:update,] do
+    resource :relationships, only: [:create, :destroy]
+    get 'followings' => 'relationships#followings', as: 'followings'
+    get 'followers' => 'relationships#followers', as: 'followers'
+  end
+  
   resources :books do
     resources :book_comments, only: [:create, :destroy]
     resource :favorites, only: [:create, :destroy]
